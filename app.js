968df688ea1a49750d3907b18f0aeb005c39fc35
Akshay Dalvi <akshaydalvi4545@gmail.com>
2019-03-03 22:41:33 -0500

    Edited app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const target = document.getElementById("calculateresults");
+
+target.addEventListener('submit',calculateResults);
+
+function calculateResults(e){
+
+  console.log("asdasd");
+  e.preventDefault();
+}
