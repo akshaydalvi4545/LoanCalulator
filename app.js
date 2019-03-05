d9bfbbf5e0a0a730d8774d5ad914102d6fa1c407
Akshay Dalvi <akshaydalvi4545@gmail.com>
2019-03-05 08:11:49 -0500

    Calculate Function

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-const target = document.getElementById("calculateresults");
+const target = document.getElementById('loan-form');
+console.log('asdasd');
 
-target.addEventListener('submit',calculateResults);
+target.addEventListener('submit',calculate)
 
-function calculateResults(e){
+function calculate(e){
+  const amount = document.getElementById('amount');
+  const interest=document.getElementById('interest');
+  const years=document.getElementById('years');
+  const monthlypay=document.getElementById('monthly-payment');
+  const totalpayment=document.getElementById('total-payment');
+  const totalInterest=document.getElementById('total-interest');
 
-  console.log("asdasd");
+
+  const principal=parseFloat(amount.value);
+  console.log(principal);
+  const calulatedInterest=parseFloat(interest.value)/100/12;
+  const totalpayments=parseFloat(years.value)*12;
+
+  const x=1/Math.pow(1+calulatedInterest,totalpayments);
+  const monthly = (principal*calulatedInterest)/(1-x);
+
+  const TOI=principal*calulatedInterest;
+  if(isFinite(monthly))
+  {
+    monthlypay.value=(monthly).toFixed(2);
+    totalpayment.value=(monthly*totalpayments).toFixed(2);;
+    totalInterest.value=((monthly*totalpayments)-principal).toFixed(2);;
+
+  }
+  else{
+
+  }
+
+  console.log(TOI);
+
+
+
+  
   e.preventDefault();
 }
